6a9031b19f37677df44f195b666b4078e3db50fb
_MadushaSandaruwan <madushakv@live.com>
2024-10-23 18:07:24 +0000

    hello world in express

---
+++ express_app/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = 4000;
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
